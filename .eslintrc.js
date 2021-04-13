dd2cd20f7128c38aca075ba13bb6425d91ec15a9
Jeremy Barnes <jeremy@wovenmagazine.com>
2021-04-13 17:40:59 -0400

    fix: updating prettier to be included in eslint file.

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,8 +32,13 @@ module.exports = {
       "prettier/prettier": [
         "warn",
         {
-          endOfLine: "auto",
-        },
+            trailingComma: 'es5',
+            singleQuote: true,
+            printWidth: 80,
+            //below line only for windows users facing CLRF and eslint/prettier error
+            // non windows users feel free to delete it
+            endOfLine: 'auto',
+          },
       ],
     },
     // overrides: [
