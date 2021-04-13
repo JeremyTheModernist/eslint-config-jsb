bf1fc871857aa866b28b859d65fac350ba4c1191
Jeremy Barnes <jeremy@wovenmagazine.com>
2021-04-13 17:38:01 -0400

    exp: trying out different prettier config.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const eslintFile = path.join(__dirname,'.eslintrc.js')
+const eslintFile = path.join(__dirname,'.eslintrc.js');
 
 const eslintrc = require(eslintFile);
 
